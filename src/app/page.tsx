4383f30a144ed60a56866569235278b2b5734361
ivandjoh <ivandi.djoh@gmail.com>
2024-09-21 18:52:04 +0800

    readme updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { Button } from "@/components/ui/button";
 
 const HomePage: React.FC = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage: React.FC = () => {
       <div className="flex flex-col items-center min-h-screen p-8 sm:p-20 bg-gray-100 dark:bg-gray-800 transition-colors duration-300">
         <main className="flex flex-col gap-8 items-center sm:items-start text-gray-900 dark:text-white">
           <h1>Hello World</h1>
+          <Button />
         </main>
       </div>
     </>
